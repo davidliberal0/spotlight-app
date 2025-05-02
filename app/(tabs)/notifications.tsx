fe6b9da0028e7e22b3c48f37207fb5ff9264bdfe
David Liberal <davidliberal0@gmail.com>
2025-05-01 21:06:08 -0400

    initial project commit with tab layout

---
+++ app/(tabs)/notifications.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "../../styles/auth.styles";
+
+const Notifications = () => {
+  return (
+    <View>
+      <Text>notifications screen</Text>
+    </View>
+  );
+};
+
+export default Notifications;
