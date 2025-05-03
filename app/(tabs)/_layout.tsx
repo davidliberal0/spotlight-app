0be5ff1db8cb0b81ca07c8f1c90a74e9abb4fc75
David Liberal <davidliberal0@gmail.com>
2025-05-02 22:15:05 -0400

    add tab bar icons and add tsx color constants export

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,51 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabLayout = () => {
   return (
     <Tabs screenOptions={{ tabBarShowLabel: false }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="bookmarks" />
-      <Tabs.Screen name="notifications" />
-      <Tabs.Screen name="create" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="home" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="bookmarks"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="bookmark" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="add-circle" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="heart" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="person-circle" size={size} color={color} />;
+          },
+        }}
+      />
     </Tabs>
   );
 };
