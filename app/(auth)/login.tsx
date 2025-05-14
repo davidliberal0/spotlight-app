460df29606bd3623008fe21f5e460660a62c0dad
David Liberal <davidliberal0@gmail.com>
2025-05-13 21:28:00 -0400

    add image to login screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,22 +1,30 @@
-import { COLORS } from "@/constants/theme"
-import { styles } from "@/styles/auth.styles"
-import Ionicons from "@expo/vector-icons/build/Ionicons"
-import React from "react"
-import { View, Text, LogBox } from "react-native"
+import { COLORS } from "@/constants/theme";
+import { styles } from "@/styles/auth.styles";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
+import React from "react";
+import { View, Text, LogBox, Image } from "react-native";
 
 const login = () => {
   return (
     <View style={styles.container}>
       {/* Brand Section */}
-        <View style={styles.brandSection}>
-          <View style={styles.logoContainer}>
-            <Ionicons name="leaf" size={32} color={COLORS.primary} /> 
-          </View>
-          <Text style={styles.appName}>spotlight</Text>
-          <Text style={styles.tagline}>don't miss anything</Text>
+      <View style={styles.brandSection}>
+        <View style={styles.logoContainer}>
+          <Ionicons name="leaf" size={32} color={COLORS.primary} />
         </View>
+        <Text style={styles.appName}>spotlight</Text>
+        <Text style={styles.tagline}>don't miss anything</Text>
+      </View>
+      {/* Illustration */}
+      <View style={styles.illustrationContainer}>
+        <Image
+          style={styles.illustration}
+          source={require("../../assets/images/Online-wishes-bro.png")}
+          resizeMode="cover"
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default login
+export default login;
