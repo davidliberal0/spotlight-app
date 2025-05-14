1c32f2228f7fd1f4f1c18f4f7b76f5295e6a954e
David Liberal <davidliberal0@gmail.com>
2025-05-13 21:53:58 -0400

    Add Google login button with terms agreement text

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -2,7 +2,7 @@ import { COLORS } from "@/constants/theme";
 import { styles } from "@/styles/auth.styles";
 import Ionicons from "@expo/vector-icons/build/Ionicons";
 import React from "react";
-import { View, Text, LogBox, Image } from "react-native";
+import { View, Text, LogBox, Image, TouchableOpacity } from "react-native";
 
 const login = () => {
   return (
@@ -22,6 +22,22 @@ const login = () => {
           source={require("../../assets/images/Online-wishes-bro.png")}
           resizeMode="cover"
         />
+      </View>
+      {/* Login Section */}
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          onPress={() => console.log("Continue with Google")}
+          activeOpacity={0.9}
+        >
+          <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={20} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <Text style={styles.termsText}>
+          By continuing, you agree to our Terms and Privacy Policy
+        </Text>
       </View>
     </View>
   );
