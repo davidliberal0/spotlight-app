04430656ad527e2c8e16ee210b7fce429172a6ee
David Liberal <davidliberal0@gmail.com>
2025-08-04 22:01:21 -0400

    start database handling, add clerk schema, and add user schema

---
+++ convex/schema.ts
@@ -0,0 +1,16 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    fullname: v.string(),
+    email: v.string(),
+    bio: v.optional(v.string()),
+    image: v.string(),
+    followers: v.number(),
+    following: v.number(),
+    posts: v.number(),
+    clerkId: v.string(),
+  }).index("by_clerkId", ["clerkId"]),
+});
